965073918f9e7d6f2fa2df4267ec0874f106d2a7
Constantine-Khoroshilov <94229399+Constantine-Khoroshilov@users.noreply.github.com>
2024-11-02 17:16:21 +0300

    Bugfix, code refactoring & additional file checks

--- server/server.py
+++ server/server.py
@@ -41,8 +41,6 @@ def generate_video_id():
 
 
 def check_video_id(video_id):
-    if video_id is None:
-        raise HTTPException(400, 'The video ID is missing')
     if video_id not in videos:
         raise HTTPException(404, 'The video ID was not found')
 
@@ -74,6 +72,11 @@ class Video:
         self.interval = None
         self.board_area = None
 
+    async def remove(self):
+        ''' Удаляет видеофайл (не объект) '''
+        await asyncio.to_thread(os.remove, self.path)
+        self.path = None
+
     def set_board_area(self, x1, y1, x2, y2):
         ''' Задает область игровой доски:
             x1, y1 - левая верхняя точка области
@@ -87,71 +90,82 @@ class Video:
         '''
         self.interval = Video.TimeInterval(start, stop)
 
-    def is_not_uploaded(self):
-        return self.status == Video.Status.NOT_UPLOADED
-
-    def is_uploading(self):
-        return self.status == Video.Status.UPLOADING
-
 
 
 @app.get('/video_id')
 async def get_video_id():
-    video_id = generate_video_id()
-    video = Video(video_id)
-    videos[video_id] = video
+    video = Video(generate_video_id())
+    videos[video.id] = video
     return {'video_id': video.id, 'status': video.status}
 
 
+class UploadingCancelled(Exception):
+    ''' Исключение, которое возникает при отмене загрузки видеофайла '''
+    def __init__(self):
+        super().__init__('Uploading was cancelled')
 
+def upload_decorator(upload_func):
+    ''' Декоратор для проверки правильности запроса перед загрузкой видеофайла '''
+    async def upload(video_id: str, file: UploadFile = File(...)):
+        check_video_id(video_id)
+        video = videos[video_id]
+
+        if not file:
+            raise HTTPException(400, 'The video file is missing')
+        if video.status != Video.Status.NOT_UPLOADED:
+            raise HTTPException(400, 'The video file has already been uploaded')
+
+        max_size = 32212254720 # 30 GB
+        if file.size > max_size:
+            raise HTTPException(400, f'The file size is more than {32212254720 // 1024**3} GB')
+        
+        extensions = {'.mp4', '.mkv', '.mov'}
+        _, extension = os.path.splitext(file.filename)
+        if extension.lower() not in extensions:
+            raise HTTPException(400, 'The extension of the file is not supported')
+
+        return await upload_func(video, file)
+
+    return upload
 
 @app.post('/upload')
-async def upload(video_id: str = None, file: UploadFile = File(None)):
-    async def remove(path):
-        await asyncio.to_thread(os.remove, path)
-
-    check_video_id(video_id)
-    if file is None:
-        raise HTTPException(400, 'The video file is missing')
-    
-    video = videos[video_id]
-    if not video.is_not_uploaded():
-        raise HTTPException(400, 'The video file has already been uploaded')
-
+@upload_decorator
+async def test(video, file):
     video.status = Video.Status.UPLOADING
-    video_path = os.path.join(videos_folder, f'{video.id}-{file.filename}')
+    video.path = os.path.join(videos_folder, f'{video.id}-{file.filename}')
     try:
-        async with aiofiles.open(video_path, 'wb') as f:
+        async with aiofiles.open(video.path, 'wb') as f:
             while True:
                 buffer = await file.read(1048576) # 1 MB
                 if not buffer:
                     break
 
-                if not video.is_uploading():
-                    await f.close()
-                    await remove(video_path)
-                    return {'video_id': video.id, 'status': video.status}
+                if video.status != Video.Status.UPLOADING:
+                    raise UploadingCancelled()
 
                 await f.write(buffer)
 
-    except Exception as e:
-        await remove(video_path)
+    except UploadingCancelled as exception:
         video.status = Video.Status.NOT_UPLOADED
-        raise HTTPException(500, f'Internal server error: {e}')
+        await video.remove()
+        return {'video_id': video.id, 'status': video.status}
+
+    except Exception as exception:
+        video.status = Video.Status.NOT_UPLOADED
+        await video.remove()
+        raise HTTPException(500, f'Internal server error: {exception}')
 
     video.status = Video.Status.UPLOADED
-    video.path = video_path
-
     return {'video_id': video.id, 'status': video.status}
 
 
 
 @app.post('/cancel_uploading')
-async def cancel_uploading(video_id: str = None):
+async def cancel_uploading(video_id: str):
     check_video_id(video_id)
     video = videos[video_id]
     
-    if not video.is_uploading():
+    if video.status != Video.Status.UPLOADING:
         raise HTTPException(400, 'The video file is not being uploaded')
 
     video.status = Video.Status.NOT_UPLOADED
