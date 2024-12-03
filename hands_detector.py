205a6a4a4539c74c3a1020197dcb8ad11f355787
Tumbleweed23 <kondroev23@gmail.com>
2024-12-03 19:57:24 +0300

    распознование руки обновлено

--- hands_detector.py
+++ hands_detector.py
@@ -1,15 +1,9 @@
 from ultralytics import YOLO
 import numpy as np
-
 def detect_hands(image: np.ndarray):
-    model = YOLO(r"..\best.pt") # путь до модели
-    results = model.predict(source=image, save=False)
+    model = YOLO(r"C:\Users\kondr\godzilla\Branch\GOdzilla\models_parameters\hands_detector\best.pt") # путь до модели
+    results = model.predict(source=image, save=True)
+    return len(results[0].boxes.conf.cpu().numpy())!= 0
 
-    arary_of_type = []
-    i = 0
-    for result in results:
-        arary_of_type.append(len(results[i].boxes.conf.cpu().numpy()) != 0)
-        i += 1
 
-    return arary_of_type[0]
 
