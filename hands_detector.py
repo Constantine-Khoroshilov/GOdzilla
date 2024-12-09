2590f0682fd97265754112888ad7cb22152765d8
Constantine-Khoroshilov <94229399+Constantine-Khoroshilov@users.noreply.github.com>
2024-12-09 14:37:55 +0300

    Bugfix stones_detector and added demo.py

--- hands_detector.py
+++ hands_detector.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 import numpy as np
 
-model = YOLO(r"models_parameters/hands_detector/best.pt")  # путь до модели
+model = YOLO(r"models_parameters/hands_detector/best.pt") # путь до модели
 
 def detect_hands(image: np.ndarray):
-    results = model.predict(source=image, save=False)
+    results = model.predict(source=image, save=False, verbose=False)
     return len(results[0].boxes.conf.cpu().numpy()) != 0
